1bb4a7b7fa60dc2f7a8a7ad1670566c280245362
reinethctonada-cmyk <reinethctonada@gmail.com>
2026-03-24 00:21:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ HTML_PAGE = """
 <div class="sidebar">
     <div class="brand">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 10v6M2 10l10-5 10 5-10 5z"/><path d="M6 12v5c3 3 9 3 12 0v-5"/></svg>
-        EduTrack Pro
+        GRADE PORTAL
     </div>
     
     <div class="form-card">
